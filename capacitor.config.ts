1ef60f9dd2ea5d99d03fdad95eeab152f851ffd7
Abdul Wasay Imran <abdulwasay905@gmail.com>
2026-08-08 05:51:58 +0500

    Added icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.awasay.koyoyomi',
-  appName: 'koyoyomi',
-  webDir: 'dist'
+    appId: "com.awasay.koyoyomi",
+    appName: "koyoyomi",
+    webDir: "dist",
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 2000,
+            launchAutoHide: true,
+            launchFadeOutDuration: 300,
+            backgroundColor: "#ffffff",
+            androidSplashResourceName: "splash",
+            showSpinner: false,
+        },
+    },
 };
 
 export default config;
